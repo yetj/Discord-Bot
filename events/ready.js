e2cf5a319cce9fc4009cfa69bcc07db0263397c3
yetj <yetj92@gmail.com>
2024-04-20 17:47:54 +0200

    added caching to reloading members on bot start

--- events/ready.js
+++ events/ready.js
@@ -1,33 +1,31 @@
 module.exports = {
-    name: "ready",
-    once: true,
-    async execute(client, client2) {
-        // load all members
-        try {
-            const promises = client2.guilds.cache.map((guild) =>
-                guild.available
-                    ? guild.members.fetch({ cache: false, force: true })
-                    : Promise.resolve()
-            );
-            await Promise.all(promises);
-        } catch (err) {
-            console.log(`Failed to fetch all members before ready! ${err}\n${err.stack}`);
-        }
+  name: "ready",
+  once: true,
+  async execute(client, client2) {
+    // load all members
+    try {
+      const promises = client2.guilds.cache.map((guild) =>
+        guild.available ? guild.members.fetch({ cache: true, force: true }) : Promise.resolve()
+      );
+      await Promise.all(promises);
+    } catch (err) {
+      console.log(`Failed to fetch all members before ready! ${err}\n${err.stack}`);
+    }
 
-        // load all channels
-        try {
-            const promises = client2.guilds.cache.map((guild) =>
-                guild.available ? guild.channels.fetch() : Promise.resolve()
-            );
-            await Promise.all(promises);
-        } catch (err) {
-            console.log(`Failed to fetch all channels before ready! ${err}\n${err.stack}`);
-        }
+    // load all channels
+    try {
+      const promises = client2.guilds.cache.map((guild) =>
+        guild.available ? guild.channels.fetch() : Promise.resolve()
+      );
+      await Promise.all(promises);
+    } catch (err) {
+      console.log(`Failed to fetch all channels before ready! ${err}\n${err.stack}`);
+    }
 
-        console.log(
-            `Ready with ${client2.guilds.cache.size} guilds, ${client2.users.cache.size} members and ${client2.channels.cache.size} channels`
-        );
+    console.log(
+      `Ready with ${client2.guilds.cache.size} guilds, ${client2.users.cache.size} members and ${client2.channels.cache.size} channels`
+    );
 
-        console.log(`Logged in as ${client2.user.tag}`);
-    },
+    console.log(`Logged in as ${client2.user.tag}`);
+  },
 };
