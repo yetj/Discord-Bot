1779a99310d8bae2694ecc695a6c3bd44fce6b06
yetj <yetj92@gmail.com>
2024-05-02 21:46:14 +0200

    new commands that allows to tag here and everyone in threads by the thread author without perms to tag everyone

---
+++ commands/here.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("here").setDescription("Ping @here in the thread."),
+  async execute(interaction) {
+    const { ownerId } = interaction.channel;
+
+    if (!interaction.channel.isThread()) {
+      return await interaction.reply({
+        content: `> *You are not allowed to use this command on this channel.*`,
+        ephemeral: true,
+      });
+    }
+
+    if (ownerId !== interaction.user.id) {
+      return await interaction.reply({
+        content: `> *Only the thread author can use this command.*`,
+        ephemeral: true,
+      });
+    }
+
+    await interaction.reply(`@here`);
+  },
+};
