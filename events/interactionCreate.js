549fc65b0addda2ec33d4b94a294cf8df8df1841
yetj <yetj92@gmail.com>
2024-07-07 00:38:37 +0200

    updated the way how interaction is created to be able to support autocomplete

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,32 +1,36 @@
 module.exports = {
-    name: "interactionCreate",
-    async execute(client, interaction) {
-        if (!interaction.isChatInputCommand()) return;
+  name: "interactionCreate",
+  async execute(client, interaction) {
+    if (!interaction.isChatInputCommand() && !interaction.isAutocomplete()) return;
 
-        console.log(
-            `>> ${interaction.user.tag} in #${interaction.channel.name} triggered an interaction ${interaction.commandName}.`
-        );
+    console.log(
+      `>> ${interaction.user.tag} in #${interaction.channel.name} triggered an interaction ${interaction.commandName}.`
+    );
 
-        const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command) return;
+    if (!command) return;
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
+    try {
+      if (interaction.isChatInputCommand()) {
+        await command.execute(interaction);
+      } else if (interaction.isAutocomplete()) {
+        await command.autocomplete(interaction);
+      }
+    } catch (error) {
+      console.error(error);
 
-            if (interaction.replied) {
-                await interaction.followUp({
-                    content: `> *There was an error while executing this command! 🙄\n> Error message: \`${error.message}\`*`,
-                    ephemeral: true,
-                });
-            } else {
-                await interaction.reply({
-                    content: `> *There was an error while executing this command! 🙄\n> *Error message: \`${error.message}\`*`,
-                    ephemeral: true,
-                });
-            }
-        }
-    },
+      if (interaction.replied) {
+        await interaction.followUp({
+          content: `> *There was an error while executing this command! 🙄\n> Error message: \`${error.message}\`*`,
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: `> *There was an error while executing this command! 🙄\n> *Error message: \`${error.message}\`*`,
+          ephemeral: true,
+        });
+      }
+    }
+  },
 };
