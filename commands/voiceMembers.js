3cc4591880868daa2704618439ee278904debd61
yetj <yetj92@gmail.com>
2024-08-14 17:58:22 +0200

    some improvements for functions

--- commands/voiceMembers.js
+++ commands/voiceMembers.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
+const getDisplayName = require("../utils/getDisplayName.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,39 +15,52 @@ module.exports = {
     .addRoleOption((option) =>
       option.setName("role").setDescription("Show only members with this role")
     )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    .addBooleanOption((option) =>
+      option.setName("show_reply").setDescription("Show reply to everyone? (default: false)")
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.MoveMembers),
   async execute(interaction) {
     const channel = interaction.options.getChannel("channel");
     const role = interaction.options.getRole("role");
+    const show_reply = interaction.options.getBoolean("show_reply") ?? false;
 
     let onlineWithRoleList = [];
 
     channel.members
-      .sort((a, b) => b.displayName - a.displayName)
+      .sort((a, b) => getDisplayName(b) - getDisplayName(a))
       .each((m) => {
         if (role) {
           if (m.roles.cache.has(role.id)) {
-            onlineWithRoleList.push(m.displayName);
+            onlineWithRoleList.push(getDisplayName(m));
           }
         } else {
-          onlineWithRoleList.push(m.displayName);
+          onlineWithRoleList.push(getDisplayName(m));
         }
       });
 
+    let message = ``;
+
     if (role) {
-      await interaction.reply(
-        `Online members on channel ${channel} in total: **${
-          channel.members.size
-        }**\nOnline members with Role **${role.name}**: ${
-          onlineWithRoleList.length
-        } \n> ${onlineWithRoleList.sort().join("\n> ")}`
-      );
+      message += `Online members on channel ${channel} in total: **${channel.members.size}**\n`;
+      message += `Online members with Role **${role.name}**: ${onlineWithRoleList.length}\n`;
+
+      if (onlineWithRoleList.length > 0) {
+        message += `> ${onlineWithRoleList.sort().join("\n> ")}`;
+      } else {
+        message += `> *No members to show*`;
+      }
+
+      await interaction.reply({ content: message, ephemeral: !show_reply });
     } else {
-      await interaction.reply(
-        `Online members on channel ${channel}: **${channel.members.size}**\n> ${onlineWithRoleList
-          .sort()
-          .join("\n> ")}`
-      );
+      message += `Online members on channel ${channel}: **${channel.members.size}**\n`;
+
+      if (onlineWithRoleList.length > 0) {
+        message += `> ${onlineWithRoleList.sort().join("\n> ")}`;
+      } else {
+        message += `> *No members to show*`;
+      }
+
+      await interaction.reply({ content: message, ephemeral: !show_reply });
     }
   },
 };
