99b3e3c6e8045a194356c227e6c1df7a7c62322d
Kamil yetj <yetj92@gmail.com>
2025-01-17 17:06:53 +0100

    added try catch to awoid restarts

--- events/memberLeftServerInfo.js
+++ events/memberLeftServerInfo.js
@@ -4,7 +4,12 @@ module.exports = {
     const chn = member.guild.systemChannel;
 
     if (chn) {
-      chn.send(`***${member.displayName}** has just left server... Bye Bye...*`);
+      try {
+        await chn.send(`***${member.displayName}** has just left server... Bye Bye...*`);
+      } catch (err) {
+        console.error("[2dde18] ", err);
+        //return await interaction.reply({ content: `>  Error . Please try again later.`, ephemeral: true });
+      }
     } else {
       console.log(`[${member.guild.name}] Can't find correct channel.`);
     }
