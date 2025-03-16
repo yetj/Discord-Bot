afefe9134f7e24a5a7b12425397baee1e3e15a28
yetj <yetj92@gmail.com>
2025-03-16 14:26:01 +0100

    Revert "finished /vacation features"
    This reverts commit 50c94139e2a749a0c958c2b501c4223ad08e1a8e.

--- commands/ex.js
+++ commands/ex.js
@@ -5,32 +5,34 @@ const {
   EmbedBuilder,
 } = require("discord.js");
 
-const Cmd1 = {
+module.exports = {
   data: new SlashCommandBuilder()
     .setName("ex")
     .setDescription("Return list of members on specific voice channel.")
     .addUserOption((option) => option.setName("user").setDescription("User")),
   async execute(interaction) {
-    console.log(interaction.options._hoistedOptions);
+    //const comm = require('../events/guildDelete.js')
+
+    //comm.execute(interaction.client, interaction.guild)
+
+    const user = interaction.options.getUser("user");
+
+    const member = interaction.guild.members.cache.find(
+      (m) =>
+        m.user.id == "165542890334978048" ||
+        m.nickname == "" ||
+        (m.user.username == "" && m.user.discriminator == "") ||
+        (m.user.globalName == "" && m.user.discriminator == "0")
+    );
+
+    console.log(member.voice);
+
+    //await interaction.deferReply({ ephemeral: true });
 
     await interaction.reply({
       content: `> ${interaction.member.nickname} - ${interaction.member.user.username}`,
     });
+
+    //console.log(interaction);
   },
 };
-
-const Cmd2 = {
-  data: new SlashCommandBuilder()
-    .setName("ex2")
-    .setDescription("Return list of members on specific voice channel.")
-    .addUserOption((option) => option.setName("user").setDescription("User")),
-  async execute(interaction) {
-    console.log(interaction.options._hoistedOptions);
-
-    await interaction.reply({
-      content: `> ${interaction.member.nickname} - ${interaction.member.user.username}`,
-    });
-  },
-};
-
-module.exports = { Cmd1, Cmd2 };
--- events/memberLeftServerInfo.js
+++ events/memberLeftServerInfo.js
@@ -1,25 +1,17 @@
-const { PermissionsBitField } = require("discord.js");
-
 module.exports = {
   name: "guildMemberRemove",
   async execute(client, member) {
     const chn = member.guild.systemChannel;
 
-    if (!chn) return console.log(`[${member.guild.name}] (LeaveInfo) System channel is not set.`);
-
-    const canSee = chn.permissionsFor(client.user).has(PermissionsBitField.Flags.ViewChannel);
-    const canWrite = chn.permissionsFor(client.user).has(PermissionsBitField.Flags.SendMessages);
-
-    if (!canSee)
-      return console.log(
-        `[${member.guild.name}] (LeaveInfo) No permissions to see System Channel.`
-      );
-
-    if (!canWrite)
-      return console.log(
-        `[${member.guild.name}] (LeaveInfo) No permissions to write on System Channel.`
-      );
-
-    chn.send(`***${member.displayName}** has just left the server... Bye Bye...*`);
+    if (chn) {
+      try {
+        await chn.send(`***${member.displayName}** has just left server... Bye Bye...*`);
+      } catch (err) {
+        console.error("[2dde18] ", err);
+        //return await interaction.reply({ content: `>  Error . Please try again later.`, ephemeral: true });
+      }
+    } else {
+      console.log(`[${member.guild.name}] Can't find correct channel.`);
+    }
   },
 };
