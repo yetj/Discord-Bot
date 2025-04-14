250029e76ffba99b07613a40e253f2066f4f7306
yetj <yetj92@gmail.com>
2025-04-14 13:30:29 +0200

    added option to create invite link

--- commands/check.js
+++ commands/check.js
@@ -1,79 +1,133 @@
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('check')
-		.setDescription('Check bot informations.')
-        .addSubcommand(subcommand => subcommand
-            .setName('servers')
-            .setDescription('Check servers status'))
-        .addSubcommand(subcommand => subcommand
-            .setName('invite')
-            .setDescription('Check invite status'))
-        .addSubcommand(subcommand => subcommand
-            .setName('leave')
-            .setDescription('Leave the server')
-            .addStringOption(option => option.setName('server').setDescription('Server ID').setRequired(true)))
-        .addSubcommand(subcommand => subcommand
-            .setName('roles_on_server')
-            .setDescription('Check number of roles on the server'))
-        .addSubcommand(subcommand => subcommand
-            .setName('channels_on_server')
-            .setDescription('Check number of channels on the server'))
-        .addSubcommand(subcommand => subcommand
-            .setName('channels_in_category')
-            .setDescription('Check number of channels in specific category')
-            .addChannelOption(option => option.setName('category').setDescription('Select category').addChannelTypes(ChannelType.GuildCategory).setRequired(true))),
-	async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName("check")
+    .setDescription("Check bot informations.")
+    .addSubcommand((subcommand) =>
+      subcommand.setName("servers").setDescription("Check servers status")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName("invite").setDescription("Check invite status")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName("inviteme").setDescription("Check invitation status")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("leave")
+        .setDescription("Leave the server")
+        .addStringOption((option) =>
+          option.setName("server").setDescription("Server ID").setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName("roles_on_server").setDescription("Check number of roles on the server")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("channels_on_server")
+        .setDescription("Check number of channels on the server")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("channels_in_category")
+        .setDescription("Check number of channels in specific category")
+        .addChannelOption((option) =>
+          option
+            .setName("category")
+            .setDescription("Select category")
+            .addChannelTypes(ChannelType.GuildCategory)
+            .setRequired(true)
+        )
+    ),
+  async execute(interaction) {
+    if (interaction.member.id !== "165542890334978048") {
+      return await interaction.reply({
+        content: `> You are not authorized to use this command!`,
+        ephemeral: true,
+      });
+    }
 
-        if (interaction.member.id !== '165542890334978048') {
-            return await interaction.reply({ content: `You are not authorized to use this command!`});
-        }
+    if (interaction.options.getSubcommand() === "servers") {
+      const guilds = interaction.client.guilds.cache;
+      let sGuilds = "";
 
-        if (interaction.options.getSubcommand() === 'servers') {
+      guilds.forEach((guild) => {
+        sGuilds += `*(${guild.id})* ${guild.name}\n`;
+      });
 
-            const guilds = interaction.client.guilds.cache
-            let sGuilds = ""
+      await interaction.reply({ content: `${sGuilds}` });
+    } else if (interaction.options.getSubcommand() === "invite") {
+      await interaction.reply({ content: `Link to invite bot:\n${interaction.client.inviteLink}` });
+    } else if (interaction.options.getSubcommand() === "inviteme") {
+      const channel = interaction.channel;
 
-            guilds.forEach(guild => {
-                sGuilds += `*(${guild.id})* ${guild.name}\n`
+      if (
+        !channel
+          .permissionsFor(interaction.client.user)
+          .has(PermissionFlagsBits.CreateInstantInvite)
+      ) {
+        return interaction.reply({
+          content: "> ❌ No permission to create invitation for this channel.",
+          ephemeral: true,
+        });
+      }
+
+      try {
+        const invite = await channel.createInvite({
+          maxAge: 0,
+          maxUses: 0,
+          unique: true,
+        });
+
+        await interaction.reply({ content: `> Invitation link: ${invite.url}`, ephemeral: true });
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({
+          content: "[f258ad] Error while checking invitatation status.",
+          ephemeral: true,
+        });
+      }
+    } else if (interaction.options.getSubcommand() === "leave") {
+      const server = interaction.options.getString("server");
+
+      try {
+        const srv = interaction.client.guilds.cache.get(server);
+
+        if (srv) {
+          srv
+            .leave()
+            .then(
+              await interaction.reply({
+                content: `Bot has left the guild: **${srv.name}** *(${srv.id})*`,
+              })
+            )
+            .catch((err) => {
+              console.log(`> [hj533]: There was an error leaving the guild: \n ${err.message}`);
             });
-
-            await interaction.reply({ content: `${sGuilds}`})
+        } else {
+          await interaction.reply({ content: `> Error: Bot is not on this guild...` });
         }
-        else if (interaction.options.getSubcommand() === 'invite') {
-            await interaction.reply({ content: `Link to invite bot:\n${interaction.client.inviteLink}`})
-        }
-        else if (interaction.options.getSubcommand() === 'leave') {
-            const server = interaction.options.getString('server')
-
-            try {
-                const srv = interaction.client.guilds.cache.get(server)
-
-                if (srv) {
-                    srv.leave().then(await interaction.reply({ content: `Bot has left the guild: **${srv.name}** *(${srv.id})*`}))
-                    .catch(err => {
-                        console.log(`> [hj533]: There was an error leaving the guild: \n ${err.message}`);
-                    })
-                }
-                else {
-                    await interaction.reply({ content: `> Error: Bot is not on this guild...`})
-                }
-            }
-            catch (e) {
-                console.log(`Error [hj534]: ${e.message}`)
-                await interaction.reply({ content: `> Error: Bot is not on this guild...`})
-            }
-        }
-        else if (interaction.options.getSubcommand() === 'roles_on_server') {
-            await interaction.reply({ content: `Roles on this server: **${interaction.guild.roles.cache.size}**`})
-        }
-        else if (interaction.options.getSubcommand() === 'channels_on_server') {
-            await interaction.reply({ content: `Channels on this server: **${interaction.guild.channels.cache.size}**`})
-        }
-        else if (interaction.options.getSubcommand() === 'channels_in_category') {
-            const category = interaction.options.getChannel('category')
-            await interaction.reply({ content: `Channels in category **${category.name}**: **${interaction.guild.channels.cache.filter(c => c.parentId === category.id).size}**`})
-        }
-	}
+      } catch (e) {
+        console.log(`Error [hj534]: ${e.message}`);
+        await interaction.reply({ content: `> Error: Bot is not on this guild...` });
+      }
+    } else if (interaction.options.getSubcommand() === "roles_on_server") {
+      await interaction.reply({
+        content: `Roles on this server: **${interaction.guild.roles.cache.size}**`,
+      });
+    } else if (interaction.options.getSubcommand() === "channels_on_server") {
+      await interaction.reply({
+        content: `Channels on this server: **${interaction.guild.channels.cache.size}**`,
+      });
+    } else if (interaction.options.getSubcommand() === "channels_in_category") {
+      const category = interaction.options.getChannel("category");
+      await interaction.reply({
+        content: `Channels in category **${category.name}**: **${
+          interaction.guild.channels.cache.filter((c) => c.parentId === category.id).size
+        }**`,
+      });
+    }
+  },
 };
